28f2d8358f374bbc69b7f6e053ffa075331ae7cd
sonu727 <119600633+sonu727@users.noreply.github.com>
2023-08-24 23:38:42 +0530

    Update basics.cpp

--- basics.cpp
+++ basics.cpp
@@ -5,3 +5,28 @@ for(int  i -> N ;i++){
 cout << ans << endl;
 ans++;
 }
+
+O();
+
+
+func(){
+for(int i->N ; i++){
+cout .....
+}
+for(int i->M ; i++){
+cout .....
+}
+}
+
+O();
+
+func(){
+for(int i->N ; i++){
+cout .....
+for(int i->M ; i++){
+cout .....
+}
+}
+}
+
+
